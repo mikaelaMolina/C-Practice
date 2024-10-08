870aef0190fa03be63c29e3e407521b56ee5a106
mikaelaMolina <159901143+mikaelaMolina@users.noreply.github.com>
2024-10-08 21:44:10 +0800

    Tried sorting algorithm
    displaying the largest and lowest values

---
+++ Molina_3 Largest Exercise.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Write a C++ program that will display the 3 largest numbers.
+*/
+
+int main(){
+	
+	int numArray;
+	
+	//Asking for how many elements the array possesses
+	cout << "Enter the number of elements in the array: " << endl;
+	cin >> numArray;
+	
+	int array[numArray]; //initialize array
+	
+	//Asking for the elements in the array
+	cout << "Enter the elements in the array: " << endl;
+	
+	//for loops for the input of the elements in the array
+	for(int i = 0; i < numArray; i++){
+		cin >> array[i];
+	}
+	
+	cout << "The 3 largest numbers in the array are: " << endl;
+	
+	//sorting the array
+	for(int j = 0; j < numArray; j++){ //these for loops go through every element
+		for(int k = 0; k < numArray; k++){
+		if(k < numArray-1){ //to make sure that it doesn't go past the number of elements in the array
+				if(array[k] > array[k+1]){ //comparing the values
+					swap(array[k], array[k+1]); //swapping the value 
+		}
+	}
+	}
+	}
+	
+	for(int i = numArray-3; i < numArray; i++){
+		cout << array[i] << endl;
+	}
+}
